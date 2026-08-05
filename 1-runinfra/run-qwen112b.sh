4f761968da4084601249ac45ae47457a4207d980
Aryan Blaauw <ablaauw@gmx.net>
2026-08-05 10:10:51 +0200

    Increase ngl parameter from 9 to 999

--- 1-runinfra/run-qwen112b.sh
+++ 1-runinfra/run-qwen112b.sh
@@ -1,3 +1,1 @@
-GGML_VK_VISIBLE_DEVICES=1,2,3,4,5 ./build/bin/llama-server   -m /media/aryan/nvme/models/Qwen3.5-122B-A10B-UD/Qwen3.5-122B-A10B-UD-IQ1_M.gguf   --tools all   -c 131072   -np 2   -ngl 9
-99   --load-mode mmap   -fa on   -t 48   -tb 48   --reasoning on   --reasoning-preserve   --host 0.0.0.0   --port 8033   --cors-origins "*"   -ctk q8_0   -ctv q4_0   --tensor-split 1,1,1,1,1   --spec-type draf
-t-mtp   --spec-draft-n-max 5
+GGML_VK_VISIBLE_DEVICES=1,2,3,4,5 ./build/bin/llama-server   -m /media/aryan/nvme/models/Qwen3.5-122B-A10B-UD/Qwen3.5-122B-A10B-UD-IQ1_M.gguf   --tools all   -c 131072   -np 2   -ngl 999   --load-mode mmap   -fa on   -t 48   -tb 48   --reasoning on   --reasoning-preserve   --host 0.0.0.0   --port 8033   --cors-origins "*"   -ctk q8_0   -ctv q4_0   --tensor-split 1,1,1,1,1   --spec-type draft-mtp   --spec-draft-n-max 5
