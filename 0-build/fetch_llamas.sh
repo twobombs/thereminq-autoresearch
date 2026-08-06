45c76a1fa7a401d4edab92d388ddcf35051c5e3b
Aryan Blaauw <ablaauw@gmx.net>
2026-08-06 11:37:13 +0200

    Add additional wget commands for model downloads

--- 0-build/fetch_llamas.sh
+++ 0-build/fetch_llamas.sh
@@ -3,6 +3,9 @@
 # cd /media/aryan/nvme/models
 
 wget https://huggingface.co/unsloth/Qwen3.6-27B-MTP-GGUF/resolve/main/Qwen3.6-27B-IQ4_XS.gguf &
+wget https://huggingface.co/unsloth/Qwen3.6-27B-MTP-GGUF/resolve/main/Qwen3.6-27B-UD-IQ3_XXS.gguf
+
 wget https://huggingface.co/unsloth/Qwen3.5-9B-MTP-GGUF/resolve/main/Qwen3.5-9B-IQ4_XS.gguf
 
-wget https://huggingface.co/bartowski/nvidia_Orchestrator-8B-GGUF/resolve/main/nvidia_Orchestrator-8B-Q6_K.gguf
+wget https://huggingface.co/bartowski/nvidia_Orchestrator-8B-GGUF/resolve/main/nvidia_Orchestrator-8B-Q6_K.gguf &
+wget https://huggingface.co/bartowski/nvidia_Orchestrator-8B-GGUF/resolve/main/nvidia_Orchestrator-8B-Q5_K_S.gguf
