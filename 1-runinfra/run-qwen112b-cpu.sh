6f154ad7f2841c2310eaa2026c1420397748bb66
Aryan Blaauw <ablaauw@gmx.net>
2026-08-07 11:53:38 +0200

    Add script to run llama-server with specific parameters

---
+++ 1-runinfra/run-qwen112b-cpu.sh
@@ -0,0 +1,1 @@
+./build/bin/llama-server   -m /media/aryan/nvme/models/Qwen3.5-122B-A10B-UD/Qwen3.5-122B-A10B-UD-IQ1_M.gguf   --tools all   -c 131072   -np 1   -ngl 0   --load-mode mmap   -fa on   -t 48   -tb 48   --reasoning on   --reasoning-preserve   --host 0.0.0.0   --port 8033   --cors-origins "*"   -ctk q8_0   -ctv q4_0   --spec-type draft-mtp   --spec-draft-n-max 5
