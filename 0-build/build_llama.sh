437d902ace4e6c442b4c7805207597db1dd8a6a2
Aryan Blaauw <ablaauw@gmx.net>
2026-08-07 11:00:35 +0200

    Add copy command for llama-vulkan-orchestrator2

--- 0-build/build_llama.sh
+++ 0-build/build_llama.sh
@@ -13,6 +13,7 @@ cd ..
 cp -r ./llama.cpp ./llama.orch
 cp -r ./llama.cpp ./llama-vulkan
 cp -r ./llama.cpp ./llama-vulkan-orchestrator
+cp -r ./llama.cpp ./llama-vulkan-orchestrator2
 cp -r ./llama.cpp ./llama-vulkan-worker1
 cp -r ./llama.cpp ./llama-vulkan-worker2
 cp -r ./llama.cpp ./llama-vulkan-worker3
