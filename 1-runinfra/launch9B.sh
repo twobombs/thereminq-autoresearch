df2655bd6938d7969a5a1eb96413a7a182c2a64f
Aryan Blaauw <ablaauw@gmx.net>
2026-08-27 16:38:49 +0000

    Update model path in launch9B.sh script

--- 1-runinfra/launch9B.sh
+++ 1-runinfra/launch9B.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-../0-build/llama.cpp/build/bin/llama-server   -m ../0-build/Qwen3.8-9B-IQ4_XS.gguf   -c 196608  --no-cache-idle-slots  -np 2   -ngl 999   --device Vulkan0   --kv-unified   -fa on   --split-mode none   --cache-type-k q8_0   --cache-type-v q4_0    --no-mmap   --spec-type draft-mtp   --spec-draft-n-max 3   --host 0.0.0.0   --port 8033   --tools all   --fit off --jinja
+../0-build/llama.cpp/build/bin/llama-server   -m ../0-build/Qwen3.8-9B-Q4_K_M.gguf   -c 196608  --no-cache-idle-slots  -np 2   -ngl 999   --device Vulkan0   --kv-unified   -fa on   --split-mode none   --cache-type-k q8_0   --cache-type-v q4_0    --no-mmap   --spec-type draft-mtp   --spec-draft-n-max 3   --host 0.0.0.0   --port 8033   --tools all   --fit off --jinja
