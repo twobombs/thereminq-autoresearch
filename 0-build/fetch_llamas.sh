1b20458b8cd5671ceacd1b78ddc9e76e341b6117
Aryan Blaauw <ablaauw@gmx.net>
2026-08-29 21:18:42 +0200

    Fix comment typo in fetch_llamas.sh

--- 0-build/fetch_llamas.sh
+++ 0-build/fetch_llamas.sh
@@ -7,9 +7,10 @@ wget https://huggingface.co/unsloth/Qwen3.8-27B-GGUF/resolve/main/Qwen3.8-27B-UD
 wget https://huggingface.co/unsloth/gemma-4-E4B-it-qat-GGUF/resolve/main/gemma-4-E4B-it-qat-UD-Q4_K_XL.gguf &
 wget https://huggingface.co/unsloth/gemma-4-E4B-it-qat-GGUF/resolve/main/MTP/mtp-gemma-4-E4B-it-Q4_0.gguf
 
-# worker node image plus 3.8 alt. distill
+# worker node image plus 3.8 alt. distills
 wget https://huggingface.co/unsloth/Qwen3.5-9B-MTP-GGUF/resolve/main/Qwen3.5-9B-IQ4_XS.gguf &
 wget https://huggingface.co/empero-ai/Qwen3.8-9B-Distill-GGUF/resolve/main/Qwen3.8-9B-Q4_K_M.gguf &
+wget https://huggingface.co/mradermacher/Qwen3.8-9B-heretic-uncensored-GGUF/resolve/main/Qwen3.8-9B-heretic-uncensored.IQ4_XS.gguf &
 
 # distilled Qwen 3.8 2B/4B for alt stitching
 wget https://huggingface.co/empero-ai/Qwen3.8-2B-Distill-GGUF/resolve/main/Qwen3.8-2B-Q4_K_M.gguf
