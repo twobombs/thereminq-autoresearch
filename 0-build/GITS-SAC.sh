18697b7a7dce6561326963a55fe05fc971f6faf8
Aryan Blaauw <ablaauw@gmx.net>
2026-08-29 21:29:47 +0200

    Update GITS-SAC.sh to download and run llama server
    Download Qwen model and start llama server with specified parameters.

--- 0-build/GITS-SAC.sh
+++ 0-build/GITS-SAC.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # this script is used when we need an in-container LLM to assist vscode to execute ThereminQuantumOPS commands
 
+cd thereminq-autoresearch/0-build/
 wget https://huggingface.co/mradermacher/Qwen3.8-9B-heretic-uncensored-GGUF/resolve/main/Qwen3.8-9B-heretic-uncensored.IQ4_XS.gguf
 /llama-vulkan/build/bin/llama-server   -m ../0-build/Qwen3.8-9B-heretic-uncensored.IQ4_XS.gguf   -c 196608  --no-cache-idle-slots  -np 2   -ngl 999   --device Vulkan0   --kv-unified   -fa on   --split-mode none   --cache-type-k q8_0   --cache-type-v q4_0    --no-mmap   --spec-type draft-mtp   --spec-draft-n-max 3   --host 0.0.0.0   --port 8033   --tools all   --fit off --jinja
