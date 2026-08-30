e795a329c108ef96a749c8d8a0ec4e4b1f1d84ab
Aryan Blaauw <ablaauw@gmx.net>
2026-08-30 13:06:31 +0200

    Update cache size for llama-server in GITS-SAC.sh
    Increased cache size for llama-server from 131072 to 196608.

--- 0-build/GITS-SAC.sh
+++ 0-build/GITS-SAC.sh
@@ -3,4 +3,4 @@
 
 cd thereminq-autoresearch/0-build/
 wget https://huggingface.co/empero-ai/Qwen3.8-9B-Distill-GGUF/resolve/main/Qwen3.8-9B-Q4_K_M.gguf
-/llama-vulkan/build/bin/llama-server   -m ../0-build/Qwen3.8-9B-Q4_K_M.gguf   -c 131072  --no-cache-idle-slots  -np 1  -ngl 999   --device Vulkan0   --kv-unified   -fa on   --split-mode none   --cache-type-k q8_0   --cache-type-v q4_0    --no-mmap   --spec-type draft-mtp   --spec-draft-n-max 3   --host 0.0.0.0   --port 8033   --tools all   --fit off --jinja
+/llama-vulkan/build/bin/llama-server   -m ../0-build/Qwen3.8-9B-Q4_K_M.gguf   -c 196608  --no-cache-idle-slots  -np 1  -ngl 999   --device Vulkan0   --kv-unified   -fa on   --split-mode none   --cache-type-k q8_0   --cache-type-v q4_0    --no-mmap   --spec-type draft-mtp   --spec-draft-n-max 3   --host 0.0.0.0   --port 8033   --tools all   --fit off --jinja
